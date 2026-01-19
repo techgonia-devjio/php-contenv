a90796e03bfdd4087bbdc7e1f3fb44ce82bd4dc9
Manpreet <m.singh@kreatif.it>
2026-01-19 09:58:02 +0100

    add ftp support

--- v2/common/build/scripts/mani-php-ext-core.sh
+++ v2/common/build/scripts/mani-php-ext-core.sh
@@ -15,6 +15,8 @@ if [ "$MODE" = "--build" ]; then
   [ "${PHP_EXT_ZIP:-false}" = "true" ]    && add "libzip-dev"
   [ "${PHP_EXT_XSL:-false}" = "true" ]    && add "libxslt1-dev"
   [ "${PHP_EXT_GMP:-false}" = "true" ]    && add "libgmp-dev"
+  # ftp
+  [ "${PHP_EXT_FTP:-false}" = "true" ]    && add "libcurl4-gnutls-dev"
 
   [ -n "$PKG" ] && { apt-get update && apt-get install -y --no-install-recommends $PKG && apt-get clean && rm -rf /var/lib/apt/lists/*; }
 
@@ -27,6 +29,8 @@ if [ "$MODE" = "--build" ]; then
   [ "${PHP_EXT_BCMATH:-false}" = "true" ] && docker-php-ext-install -j"$(nproc)" bcmath || true
   [ "${PHP_EXT_EXIF:-false}" = "true" ]   && docker-php-ext-install -j"$(nproc)" exif || true
   [ "${PHP_EXT_PCNTL:-false}" = "true" ]  && docker-php-ext-install -j"$(nproc)" pcntl || true
+  [ "${PHP_EXT_FTP:-false}" = "true" ]    && docker-php-ext-install -j"$(nproc)" ftp || true
+
 
   # Xdebug: install only; ini controls activation at runtime
   [ "${PHP_EXT_XDEBUG:-true}" = "true" ] && pecl install xdebug || true
@@ -56,6 +60,9 @@ add(){ echo " $PKG " | grep -q " $1 " || PKG="$PKG $1"; }
 # If Xdebug disabled, drop ini to avoid loader warning
 [ "${PHP_EXT_XDEBUG:-true}" = "true" ] || rm -f /usr/local/etc/php/conf.d/92-docker-php-ext-xdebug.ini || true
 
-
+# install ftp and configuew with ssl  and curl
+[ "${PHP_EXT_FTP:-false}" = "true" ] && {
+  docker-php-ext-configure ftp --with-ftp-ssl
+}
 
 echo "----> Core PHP extensions installed successfully."
